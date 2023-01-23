f1ec80dde835e8542c8de48b31586b7f5f828579
Eyem <eyem@Eyems-MacBook-Air.local>
2023-01-23 15:39:17 +0700

    Initial Files.

---
+++ example/qurano_scrapper_example.dart
@@ -0,0 +1,6 @@
+import 'package:qurano_scrapper/qurano_scrapper.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/qurano_scrapper.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library qurano_scrapper;
+
+export 'src/qurano_scrapper_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
